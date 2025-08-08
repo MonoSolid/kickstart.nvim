2915391c9c3b7135935b167406164fe8d5ceb92c
Josef Sturzenbecker <josef.sturzenbecker@gmail.com>
2025-08-08 12:13:14 +0200

    Cleaned up neovim config.

---
+++ lua/block/plugins/akinsho_toggleTerm.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    size = 20,
+    hide_numbers = true,
+    open_mapping = [[<c-\>]],
+    direction = 'float',
+  },
+  keys = {},
+}
