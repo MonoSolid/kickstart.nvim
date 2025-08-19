82d5701be249e146fea895b58d751dcde8f63090
MonoSolid <josef.sturzenbecker@gmail.com>
2025-08-19 20:06:56 +0200

    toggle term basics. works fine.

--- lua/block/plugins/akinsho_toggleTerm.lua
+++ lua/block/plugins/akinsho_toggleTerm.lua
@@ -1,11 +1,27 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {
-    size = 20,
-    hide_numbers = true,
-    open_mapping = [[<c-\>]],
-    direction = 'float',
-  },
-  keys = {},
+  config = function()
+    -- Start commonly used terminals at first toggle.
+    local isInit = false
+    local function runCommonTerminals()
+      if isInit then
+        return
+      end
+      isInit = true
+      vim.cmd ':2ToggleTerm'
+      vim.cmd ':2ToggleTerm'
+      vim.cmd ':1TermExec cmd="lazygit"'
+      vim.cmd ':1ToggleTerm'
+    end
+
+    require('toggleterm').setup {
+      open_mapping = [[<c-\>]],
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+      },
+      on_open = runCommonTerminals,
+    }
+  end,
 }
