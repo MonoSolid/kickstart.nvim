13d34905dc50b3ec1ffde405566d28213982b9f3
monosolid <josef.sturzenbecker@gmail.com>
2026-03-22 19:23:46 +0000

    minor edit

--- lua/block/plugins/copilotC-Nvim_CopilotChat.lua
+++ lua/block/plugins/copilotC-Nvim_CopilotChat.lua
@@ -6,15 +6,13 @@ return {
     },
     build = 'make tiktoken',
     opts = {
-      {
-        model = 'GPT-5.3-Codex', -- AI model to use
-        temperature = 0.1, -- Lower = focused, higher = creative
-        window = {
-          layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-          width = 0.5, -- 50% of screen width
-        },
-        auto_insert_mode = true, -- Enter insert mode when opening
+      model = 'GPT-5.3-Codex', -- AI model to use
+      temperature = 0.1, -- Lower = focused, higher = creative
+      window = {
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float'
+        width = 0.5, -- 50% of screen width
       },
+      auto_insert_mode = true, -- Enter insert mode when opening
     },
   },
 }
